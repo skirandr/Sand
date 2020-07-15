05d01c503ab013f3fd553de762b6339da602c7f1
skirandr <68293109+skirandr@users.noreply.github.com>
2020-07-15 20:46:12 +0530

    Update poi_1.java

--- src/poi/poi_1.java
+++ src/poi/poi_1.java
@@ -3,7 +3,10 @@ package poi;
 public class poi_1 {
 
 	public static void main(String[] args) {
-		System.out.println("sand");
+		
+		System.out.println("sand1");
+		System.out.println("sand2");
+		System.out.println("sand3");
 
 	}
 
